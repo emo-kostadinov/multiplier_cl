cb6f718537c6cca264e101740cb90e0dd6dca340
emo-kostadinov <emilgold06@gmail.com>
2025-05-02 21:14:04 +0300

    autofix

--- multiplier/tests/Unit/MultiplyByTenTest.php
+++ multiplier/tests/Unit/MultiplyByTenTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . ' / ' . '../../../src/multiplyBy10.php';
+require_once __DIR__.'/../../../src/multiplyBy10.php';
 
 use PHPUnit\Framework\TestCase;
 
