16a5dcc9b247b3084fd0ad9a4d0f2e4a2e46e35d
emo-kostadinov <emilgold06@gmail.com>
2025-05-03 00:40:40 +0300

    fixes

--- multiplier/tests/TestCase.php
+++ multiplier/tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
+class TestCase extends BaseTestCase
 {
-    //
+    // Shared setup can go here later
 }
